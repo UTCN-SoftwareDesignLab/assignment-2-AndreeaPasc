d9163ff432af018b8da6ddef74aad80a6014d2a0
Andreea Madalina Pasc <andreea_pasc@yahoo.com>
2021-04-20 22:08:06 +0300

    pdf + csv service

--- Assignments/Assignment 2/backend/src/main/java/com/assignment2/report/CSVReportService.java
+++ Assignments/Assignment 2/backend/src/main/java/com/assignment2/report/CSVReportService.java
@@ -1,11 +1,37 @@
 package com.assignment2.report;
 
+import com.assignment2.book.model.Book;
+import com.assignment2.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.assignment2.report.ReportType.CSV;
 
+@RequiredArgsConstructor
 @Service
 public class CSVReportService implements ReportService {
+
+    private final BookRepository bookRepository;
+
+    public void csvGenerate() throws IOException {
+        List<Book> books = bookRepository.findAll().stream().filter(book -> book.getQuantity() == 0).collect(Collectors.toList());
+
+        try (PrintWriter writer = new PrintWriter(new File("PrettyCSV.csv"))) {
+
+            for (Book book : books) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(book.getId().toString()).append(book.getTitle()).append(book.getAuthor()).append(book.getPrice().toString()).append(book.getQuantity()).append('\n');
+                writer.write(sb.toString());
+            }
+        }
+    }
+
     @Override
     public String export() {
         return "I am a CSV reporter.";
--- Assignments/Assignment 2/backend/src/main/java/com/assignment2/report/PdfReportService.java
+++ Assignments/Assignment 2/backend/src/main/java/com/assignment2/report/PdfReportService.java
@@ -1,11 +1,44 @@
 package com.assignment2.report;
 
+import com.assignment2.book.model.Book;
+import com.assignment2.book.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.assignment2.report.ReportType.PDF;
 
+@RequiredArgsConstructor
 @Service
 public class PdfReportService implements ReportService {
+
+    private final BookRepository bookRepository;
+
+    public void pdfGenerate() throws IOException {
+        List<Book> books = bookRepository.findAll().stream().filter(book -> book.getQuantity() == 0).collect(Collectors.toList());
+
+        PDDocument doc = new PDDocument();
+        PDPage page = doc.getPage(1);
+        PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+
+        contentStream.beginText();
+        contentStream.newLineAtOffset(20, 450);
+        for(Book book: books) {
+            contentStream.showText(book.toString());
+            contentStream.newLine();
+        }
+        contentStream.endText();
+
+        doc.save("PrettyPDF");
+        doc.close();
+    }
+
     @Override
     public String export() {
         return "I am a PDF reporter.";
